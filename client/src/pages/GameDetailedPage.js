88177c754f6a1713dc4f03974c3c89e8892c1fe2
Loi Van Tran <datloiboi@gmail.com>
2021-06-15 11:04:39 +0700

    Create example pages

---
+++ client/src/pages/GameDetailedPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function GameDetailedPage() {
+  // GET http://www.production.com/api/games/:id
+  return (
+    <div>
+      <h1>Game Detail</h1>
+    </div>
+  )
+}
